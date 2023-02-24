5a4430c85c86c98406c8b4e192f0ba3471fe5756
jason <j450nx@gmail.com>
2023-02-24 13:22:40 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium.Edge;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace SeleniumTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("test case started.");
+            IWebDriver driver = new EdgeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            IWebElement searchBox = driver.FindElement(By.Name("q"));
+            searchBox.SendKeys("javatpoint tutorial");
+            IWebElement searchButton = driver.FindElement(By.Name("btnK"));
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+            searchButton.Click();
+            driver.Close();
+            driver.Quit();
+            Console.Write("test case ended.");
+        }
+    }
+}
---
+++ Sample1.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium.Edge;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+namespace SeleniumTest
+{
+    internal class Sample1
+    {
+        IWebDriver driver = new EdgeDriver();
+        [SetUp]
+        public void Initialize()
+        {
+            driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/");
+        }
+        [Test]
+        public void ExecuteTest()
+        {
+            IWebElement basicAuth = driver.FindElement(By.CssSelector("#content > ul > li:nth-child(3) > a"));
+            basicAuth.Click();
+            driver.SwitchTo().Window(driver.WindowHandles.Last());
+        }
+        [TearDown]
+        public void EndTest()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
